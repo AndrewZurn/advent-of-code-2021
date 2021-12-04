272073cc51f76cbbd966802c6f45d09be5b1987b
AndrewZurn <awzurn@gmail.com>
2021-12-03 23:23:10 -0800

    Day 2 Part 2 Clojure

--- clojure/advent-of-code/src/advent_of_code/day2.clj
+++ clojure/advent-of-code/src/advent_of_code/day2.clj
@@ -11,19 +11,36 @@
          :value     (Integer/parseInt (last split))}))
     data))
 
-(defn answer-it [coordinates]
+(defn calculate-next-position [next-direction next-value horizontal depth]
+  (if (= "forward" next-direction)
+    {:horizontal (+ horizontal next-value) :depth depth}
+    (if (= "up" next-direction)
+      {:horizontal horizontal :depth (- depth next-value)}
+      ;; else down
+      {:horizontal horizontal :depth (+ depth next-value)})))
+
+(defn calculate-next-position-with-aim [next-direction next-value horizontal depth aim]
+  (if (= "forward" next-direction)
+    {:horizontal (+ horizontal next-value) :depth (+ depth (* aim next-value)) :aim aim}
+    (if (= "up" next-direction)
+      {:horizontal horizontal :depth depth :aim (- aim next-value)}
+      ;; else down
+      {:horizontal horizontal :depth depth :aim (+ aim next-value)})))
+
+(defn answer-it [coordinates with-aim]
   (reduce (fn [accum elem]
             (let [horizontal (:horizontal accum)
                   depth (:depth accum)
+                  aim (:aim accum 0)                        ;; default 0
                   next-direction (:direction elem)
                   next-value (:value elem)]
-              (if (= "forward" next-direction)
-                {:horizontal (+ horizontal next-value) :depth depth}
-                (if (= "up" next-direction)
-                  {:horizontal horizontal :depth (- depth next-value)}
-                  {:horizontal horizontal :depth (+ depth next-value)}))))
-          {:horizontal 0 :depth 0}                          ;; initial element
+              (if (true? with-aim)
+                (calculate-next-position-with-aim next-direction next-value horizontal depth aim)
+                (calculate-next-position next-direction next-value horizontal depth))))
+          {:horizontal 0 :depth 0 :aim 0}                   ;; initial element
           coordinates))
 
-(def answer-one (answer-it parse-data))
+(def answer-one (answer-it parse-data false))
+(def answer-two (answer-it parse-data true))
 (println "Part 1 total was: " (* (:horizontal answer-one) (:depth answer-one)))
+(println "Part 2 total was: " (* (:horizontal answer-two) (:depth answer-two)))
