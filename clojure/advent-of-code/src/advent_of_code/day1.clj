0d899ea6ceb3d4f8e419ca39dcfe3380b3c0cd51
AndrewZurn <awzurn@gmail.com>
2021-12-03 17:18:53 -0800

    Day 1 - Clojure

---
+++ clojure/advent-of-code/src/advent_of_code/day1.clj
@@ -0,0 +1,27 @@
+(ns advent-of-code.day1
+  (:require [clojure.java.io :as io]))
+
+(def data (io/resource "day1.txt"))
+
+(def parsed-numbers
+  (map #(Integer/parseInt %)
+       (.split (slurp data) "\n")))
+
+(defn answer-it-all [nums]
+  (reduce
+    (fn [accum elem]
+      (let [previous-elem (:previous-elem accum)
+            total (:total accum)]
+        (if (> elem previous-elem)
+          {:total (+ total 1) :previous-elem elem}
+          {:total total :previous-elem elem})))
+    {:total 0 :previous-elem 999999}                        ;; initial element, previous-elem should be a large number so we don't count the first comparison (ie any number is always greater than 0/null)
+    nums))
+
+(def grouped-inputs-summed
+  (map
+    (fn [group] (reduce + group))
+    (partition 3 1 parsed-numbers)))
+
+(println "Part 1 total was:" (:total (answer-it-all parsed-numbers)))
+(println "Part 2 total was:" (:total (answer-it-all grouped-inputs-summed)))
